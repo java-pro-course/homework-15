631f01216c31e4dfd4a4266d56bfd878e4065cfe
Svarog916 <kau.ninja916@gmail.com>
2022-11-15 19:25:46 +0300

    add 2

---
+++ SH/2/demo/src/main/java/com/koritskiy/demo/api/Controller.java
@@ -0,0 +1,27 @@
+package com.koritskiy.demo.api;
+
+import com.koritskiy.demo.service.FirstService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    public Controller(FirstService service) {
+        this.service = service;
+    }
+
+    private FirstService service;
+
+    @GetMapping("/multiply/{firstNumber}/{secondNumber}")
+    public int multiply(@PathVariable int firstNumber,@PathVariable int secondNumber){
+        return service.multiply(firstNumber, secondNumber);
+    }
+
+    @GetMapping("dividers")
+    public String  dividers (@RequestParam int number){
+        return service.divs(number);
+    }
+}
