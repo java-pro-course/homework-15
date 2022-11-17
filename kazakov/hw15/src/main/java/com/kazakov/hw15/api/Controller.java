f402e55cae576e684910cd905f470437272f54b8
chewedmole <chewedmole.git@gmail.com>
2022-11-17 14:32:10 +0700

    Homework 15. All tasks in Controller, all methods in Service.

--- kazakov/hw15/src/main/java/com/kazakov/hw15/api/Controller.java
+++ kazakov/hw15/src/main/java/com/kazakov/hw15/api/Controller.java
@@ -1,6 +1,6 @@
 package com.kazakov.hw15.api;
 
-import com.kazakov.hw15.services.Service;
+import com.kazakov.hw15.service.Service;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,14 +11,36 @@ public class Controller {
         this.service = service;
     }
 
-    @GetMapping("multiply/{firstNumber}/{secondNumber}")
+    @GetMapping("multiply/{firstNumber}/{secondNumber}") // task1
     public int getMult(@PathVariable int firstNumber, @PathVariable int secondNumber) {
         return service.multiply(firstNumber, secondNumber);
     }
 
-    @GetMapping("dividers")
-    @ResponseBody
+    @GetMapping("dividers") // task2
     public String getDiv(@RequestParam("number") int num) {
         return service.findDividers(num);
     }
+
+    @GetMapping("string/{strVal}") // task3
+    public String getString(@PathVariable String strVal){
+        return service.printStr(strVal);
+    }
+
+    @GetMapping("user/{name}/{surname}/{age}")
+    public String getUser(@PathVariable String name, @PathVariable String surname, @PathVariable int age){
+        return service.getUser(name, surname, age);
+    }
+
+
+
+
+//    @GetMapping("string/{strVal}")                                //question
+//    public String getString(@PathVariable String strVal){
+//        String[] result = strVal.split("");
+//        for (int i = 0; i < strVal.length(); i++) {
+//            return i + " - " + result[i];
+//        }
+//        return "";
+//    }
+
 }
